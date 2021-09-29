c05964b71f2b039d3532096bc085af4bd40fd5ce
github username <github email address>
2021-09-29 15:51:41 -0400

    commented out possible solution, only works for the first recipe, working on possible solution

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,12 +1,12 @@
 class RecipesController < ApplicationController
     def index
-        @recipes = Recipe.all
+        recipes = Recipe.all
         render json: recipes, status: :ok
     end
     
     def show 
-        @recipe = find_recipe
-        render json: @recipe, status: :ok
+        recipe = find_recipe
+        render json: recipe, status: :ok
     end
 
     private 
--- app/serializers/ingredient_serializer.rb
+++ app/serializers/ingredient_serializer.rb
@@ -1,17 +1,14 @@
 class IngredientSerializer < ActiveModel::Serializer
-  attributes :id, :name, :image, :quantity, :measure_unit
-  
+  # attributes :id, :name, :image
+  # attributes :id, :name, :image, :quantity
   has_many :recipes
 
-  def quantity
-    recipe =  Recipe.joins(recipe_ingredients: :ingredient)
-    quantity = recipe.first.recipe_ingredients.where(id: self.object.id).first.quantity
-    quantity 
-    # .where(ingredients: self.object.id).ingredients.first.quantity
-  end
-  def measure_unit
-    recipe =  Recipe.joins(recipe_ingredients: :ingredient)
-    measure_unit = recipe.first.recipe_ingredients.where(id: self.object.id).first.measure_unit
-    measure_unit 
-  end
+  # def quantity
+  ## Joined tabled but only works for the first recipe 
+  #   recipe =  Recipe.joins(recipe_ingredients: :ingredient)
+  #   quantity = recipe.first.recipe_ingredients.where(id: self.object.id).first.quantity
+  #   byebug
+  #   quantity 
+  #   .where(ingredients: self.object.id).ingredients.first.quantity
+  # end
 end
