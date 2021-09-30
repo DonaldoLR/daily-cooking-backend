68642de18f8794ccfe03dc1f911f5e9d746ed14e
github username <donaldolr0206@gmail.com>
2021-09-30 14:48:24 -0400

    Added ingredients_with_quantities attribute to Recipe Serializer, created a helper method to find the additional data and append it to the ingredient data

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,6 +1,18 @@
 class RecipeSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :image, :instructions 
+  attributes :id, :name, :description, :image, :instructions, :ingredients_with_quantities 
 
   has_many :ingredients
+
+  def ingredients_with_quantities
+    ingredients = self.object.ingredients.includes(:recipe_ingredients)
+     
+    ingredients_array = ingredients.map do |ingredient| 
+        additional_data = ingredient.recipe_ingredients.where(recipe_id: self.object.id).limit(1).first
+        quantity = additional_data.ingredient_quantity
+        ingredient_description = additional_data.ingredient_description
+        {name: ingredient.name, image: ingredient.image, quantity: quantity, ingredient_description: ingredient_description}
+    end
+    ingredients_array
+  end
 end
 #
