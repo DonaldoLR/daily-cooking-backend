4b9bbba14a7f6ac373503c296db5d920c10559a3
github username <donaldolr0206@gmail.com>
2021-10-15 14:23:22 -0400

    updated seeds and migration fil

--- db/migrate/20210928113447_create_recipe_ingredients.rb
+++ db/migrate/20210928113447_create_recipe_ingredients.rb
@@ -3,7 +3,7 @@ class CreateRecipeIngredients < ActiveRecord::Migration[6.1]
     create_table :recipe_ingredients do |t|
       t.belongs_to :recipe, foreign_key: true
       t.belongs_to :ingredient, foreign_key: true
-      t.decimal :ingredient_quantity
+      t.decimal :ingredient_quantity, :null => false, :default =>  0
       t.string :ingredient_description
       t.timestamps
     end
